297d694c8376bd93aafe4f8cdf19f1d79e16d222
olleth <olleth@kth.se>
2022-09-19 10:41:32 +0200

    Create and add chess-template to lib

---
+++ Chess/target/debug/deps/Chess-97fdc3a6f6d89cd7.d
@@ -0,0 +1,5 @@
+/home/olleth/rust-assignments/olleth-chess/Chess/target/debug/deps/Chess-97fdc3a6f6d89cd7.rmeta: src/lib.rs
+
+/home/olleth/rust-assignments/olleth-chess/Chess/target/debug/deps/Chess-97fdc3a6f6d89cd7.d: src/lib.rs
+
+src/lib.rs:
---
+++ Chess/target/debug/deps/Chess-e581cefe7bb4d993.d
@@ -0,0 +1,5 @@
+/home/olleth/rust-assignments/olleth-chess/Chess/target/debug/deps/Chess-e581cefe7bb4d993.rmeta: src/lib.rs
+
+/home/olleth/rust-assignments/olleth-chess/Chess/target/debug/deps/Chess-e581cefe7bb4d993.d: src/lib.rs
+
+src/lib.rs:
