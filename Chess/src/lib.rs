e781cacf44c24e721aed2bea3f7567e25bbe5ff1
olleth <olleth@kth.se>
2022-09-29 22:35:44 +0200

    fixed moves, sliding pieces can move through other pieces

--- Chess/src/lib.rs
+++ Chess/src/lib.rs
@@ -79,8 +79,8 @@ impl Board {
         let mut squares = [[Square { piece: None }; 8]; 8];
     
         for x in 0..8 {
-            squares[x][1].piece = Some(Piece::WPawn);
-            squares[x][6].piece = Some(Piece::BPawn);
+            squares[x][6].piece = Some(Piece::WPawn);
+            squares[x][1].piece = Some(Piece::BPawn);
             let wpiece = match x {
                 0 | 7 => Some(Piece::WRook),
                 1 | 6 => Some(Piece::WKnight),
@@ -97,8 +97,8 @@ impl Board {
                 4 => Some(Piece::BQueen),
                 _ => unreachable!()
             };
-            squares[x][0].piece = wpiece;
-            squares[x][7].piece = bpiece;
+            squares[x][7].piece = wpiece;
+            squares[x][0].piece = bpiece;
         }
     
         Board {
@@ -115,48 +115,28 @@ impl Board {
         let mut from_sq = chessboard.squares[Board::file_index(from.0)][Board::rank_index(from.1)];
         let mut to_sq = chessboard.squares[Board::file_index(to.0)][Board::rank_index(to.1)];
 
-        println!("{:?}", from_sq.piece.unwrap());
-
-
-
         if (Board::legal_moves(to_sq, from_sq.piece.unwrap(), from, to) == true) {
             if WHITE.contains(&from_sq.piece) { 
-                (from_sq, to_sq) = Board::white_move(from_sq, to_sq);
-                chessboard.squares[Board::file_index(from.0)][Board::rank_index(from.1)] = from_sq;
-                chessboard.squares[Board::file_index(to.0)][Board::rank_index(to.1)] = to_sq;
-            } 
-            else {(from_sq, to_sq) = Board::black_move(from_sq, to_sq); } 
-                chessboard.squares[Board::file_index(from.0)][Board::rank_index(from.1)] = from_sq;
-                chessboard.squares[Board::file_index(to.0)][Board::rank_index(to.1)] = to_sq;
+                if  WHITE.contains(&to_sq.piece)  {
+                    println!("Square is occupied by a white piece!");
+                }
+                else {
+                    println!("{:?}", chessboard.squares[Board::file_index(from.0)][Board::rank_index(from.1)].piece);
+                    chessboard.squares[Board::file_index(from.0)][Board::rank_index(from.1)].piece = None;
+                    chessboard.squares[Board::file_index(to.0)][Board::rank_index(to.1)].piece = from_sq.piece;
+                 } 
+            }
+            if BLACK.contains(&from_sq.piece) {
+                if  BLACK.contains(&to_sq.piece)  {
+                    println!("Square is occupied by a white piece!");
+                }
+                else {
+                    chessboard.squares[Board::file_index(from.0)][Board::rank_index(from.1)].piece = None;
+                    chessboard.squares[Board::file_index(to.0)][Board::rank_index(to.1)].piece = from_sq.piece;
+                 } 
+            }
         }
-    
-        return chessboard
-    }
-
-    pub fn white_move(mut f: Square, mut t: Square) -> (Square, Square) {
-        if  WHITE.contains(&t.piece)  {
-            println!("Square is occupied by a white piece!");
-            return (f, t)
-        }
-        else {
-            f.piece = None;
-            t.piece = f.piece;
-            return (f, t)
-
-        }
-    }
-
-    pub fn black_move(mut f: Square, mut t: Square) -> (Square, Square) {
-        if  BLACK.contains(&t.piece)  {
-            println!("Square is occupied by a white piece!");
-            return (f, t)
-        }
-        else {
-            f.piece = None;
-            t.piece = f.piece;
-            return (f, t)
-
-        }
+        return chessboard;
     }
 
     pub fn file_index(file: &str) -> usize {
@@ -164,7 +144,7 @@ impl Board {
     }
 
     pub fn rank_index(rank: &str) -> usize {
-        return (rank.parse::<usize>().unwrap()-1);
+        return RANKS.iter().position(|&rs| rs == rank.parse::<usize>().unwrap()).unwrap();
     }
 
     pub fn get_from(dir: &str) -> (&str, &str) {
@@ -185,40 +165,36 @@ impl Board {
 
         let file_average = file1 - file2;
         let rank_average = rank1 - rank2;
-    
+
+        println!("{}", rank_average);
+
         let mut legal = false; 
 
-        if (p == Piece::BPawn) || (p == Piece::WPawn) { legal = Board::pawn_valid(sq, p, file_average, rank_average); }
+        if (p == Piece::BPawn) || (p == Piece::WPawn) { legal = Board::pawn_valid(sq, p, file_average, rank_average, f); }
         if (p == Piece::BRook) || (p == Piece::WRook) { legal = Board::rook_valid(sq, p, file_average, rank_average); }
         if (p == Piece::BBishop) || (p == Piece::WBishop) { legal = Board::bishop_valid(sq, p, file_average, rank_average); }
-        if (p == Piece::BKnight) || (p == Piece::WKnight) { 
-            legal = Board::knight_valid(sq, p, file_average, rank_average); 
-            println!("{:?}", legal);
-        }
+        if (p == Piece::BKnight) || (p == Piece::WKnight) { legal = Board::knight_valid(sq, p, file_average, rank_average); }
         if (p == Piece::BQueen) || (p == Piece::WQueen) { legal = Board::queen_valid(sq, p, file_average, rank_average); }
         if (p == Piece::BKing) || (p == Piece::WKing) { legal = Board::king_valid(sq, p, file_average, rank_average); } 
+
+        println!("{}", legal);
 
         return legal;
     }
 
-    fn pawn_valid(sq: Square, p: Piece, f: i32, r: i32, ) -> bool {
-        if (p == Piece::WPawn) && (BLACK.contains(&sq.piece)) && (f == 1 && f == -1) {
-            return true 
-        }
-        //if (p == Piece::WPawn) & (Board::file_index(t.0) == 1) & (r == 2) {
-            //return true
-        //}
-        if (p == Piece::WPawn) & (r == 1) {
-            return true
-        }
-        if (p == Piece::BPawn) & (WHITE.contains(&sq.piece)) && (f == 1 && f == -1) {
-            return true
-        } 
-        else { return false }
+    fn pawn_valid(sq: Square, p: Piece, f: i32, r: i32, from: (&str, &str)) -> bool {
+        let mut legals = false;
+        if (p == Piece::WPawn) && (BLACK.contains(&sq.piece)) && (f == 1 && r == -1) && (f == -1 && r == -1) { legals = true; }
+        if (p == Piece::WPawn) && (Board::rank_index(from.1) == 6) && (r == -2) {  legals = true; }
+        if (p == Piece::WPawn) && (r == -1) {  legals = true; }
+        if (p == Piece::BPawn) && (WHITE.contains(&sq.piece)) && (f == 1 && r == 1) && (f == -1 && r == 1) {  legals = true;  }
+        if (p == Piece::BPawn) && (Board::rank_index(from.1) == 1) && (r == 2) {  legals = true; }
+        if (p == Piece::BPawn) && (r == 1) {  legals = true; }
+    
+        return legals;
     }
     fn rook_valid(sq: Square, p: Piece, f: i32, r: i32) -> bool {
-        return ((p == Piece::WRook) || (p == Piece::BRook))  && ((f <=7 && f >= -7 && r == 0) || (r <=7 && r >= -7 && f == 0));
-    
+        return ((p == Piece::WRook) || (p == Piece::BRook)) && ((f <=7 && f >= -7 && r == 0) || (r <= 7 && r >= -7 && f == 0));
     }
     fn bishop_valid(sq: Square, p: Piece, f: i32, r: i32) -> bool {
         return ((p == Piece::WBishop) || (p == Piece::BBishop)) && (f == r);
@@ -238,7 +214,7 @@ impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut res = String::new();
         let mut rank = [" 8", " 7", " 6", " 5", " 4", " 3", " 2", " 1"];
-        let mut file = "\nA B C D E F G H";
+        let mut file = " A B C D E F G H";
         for i in 0..8 {
             for j in 0..8 {
                 res.push_str(self.squares[j][i].symbol());
@@ -253,8 +229,9 @@ impl fmt::Display for Board {
     }
 }
 
-const FILES: &'static [&'static str] = &["h", "g", "f", "e", "d", "c", "b", "a"];
+const FILES: &'static [&'static str] = &["a", "b", "c", "d", "e", "f", "g", "h"];
+const RANKS: [usize; 8] = [8,7,6,5,4,3,2,1];
 
-pub const WHITE:[Option<Piece>; 5] = [Some(Piece::WKing) , Some(Piece::WQueen) , Some(Piece::WBishop) , Some(Piece::WKnight) , Some(Piece::WPawn)];
-pub const BLACK:[Option<Piece>; 5] = [Some(Piece::BKing) , Some(Piece::BQueen) , Some(Piece::BBishop) , Some(Piece::BKnight) , Some(Piece::BPawn)];
+pub const WHITE:[Option<Piece>; 6] = [Some(Piece::WKing) , Some(Piece::WQueen) , Some(Piece::WBishop) , Some(Piece::WKnight), Some(Piece::WRook), Some(Piece::WPawn)];
+pub const BLACK:[Option<Piece>; 6] = [Some(Piece::BKing) , Some(Piece::BQueen) , Some(Piece::BBishop) , Some(Piece::BKnight), Some(Piece::BRook), Some(Piece::BPawn)];
 
